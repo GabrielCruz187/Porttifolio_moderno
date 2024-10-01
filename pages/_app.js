ea2abe25f273ee59132bdb59ca58bd372393a618
Gabriel Cruz <gabicraque10@outlook.com>
2024-09-30 22:06:56 -0300

    play in project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css';
 
+import Layout from '../components/Layout';
+import Transition from '../components/Transition'
+
+import { useRouter } from  'next/router'
+
+import { AnimatePresence, motion } from 'framer-motion';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <div>
+          <Transition />
+          <Component {...pageProps} />
+        </div>
+      </AnimatePresence>
+     
+     </Layout>
+  );
 }
 
 export default MyApp;
