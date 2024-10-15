bc9d90e83bee257d8bbe5e940436e72f5d2a5b34
Gabriel Cruz <gabicraque10@outlook.com>
2024-10-14 22:03:32 -0300

    adicionando o lunk das redes sociais e dos projetos

--- components/Socials.js
+++ components/Socials.js
@@ -10,13 +10,13 @@ import {
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://www.instagram.com/gabriell___cruzz/'} className="hover:text-accent transition-all duration-300">
         <RiInstagramLine />
       </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://www.linkedin.com/in/gabriel-silva-cruz-67160526a/'} className="hover:text-accent transition-all duration-300">
         <RiLinkedinBoxLine />
       </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://github.com/GabrielCruz187'} className="hover:text-accent transition-all duration-300">
         <RiGithubLine />
       </Link>
       
