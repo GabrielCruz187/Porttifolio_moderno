a19f6d49b8ebeff45943976cc0c0c442109ee02a
Gabriel Cruz <gabicraque10@outlook.com>
2025-02-19 13:51:41 -0300

    resolvendo erro na hora do deploy

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -3,59 +3,61 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper';
 import Image from 'next/image';
-import { FaReact, FaNodeJs, FaCss3Alt, FaHtml5, FaCss3, FaJs, } from 'react-icons/fa'; // Exemplos de ícones
-import { SiTailwindcss, SiNextdotjs, SiSqlalchemy, SiSqlite } from 'react-icons/si'; // Outros ícones
+import { FaReact, FaNodeJs, FaCss3Alt, FaHtml5, FaCss3, FaJs } from 'react-icons/fa';
+import { SiTailwindcss, SiNextdotjs, SiSqlalchemy, SiSqlite } from 'react-icons/si';
 
-// Dados do slider
 const workSlides = [
   {
+    id: 1,
     title: 'Restaurante',
     path: '/food.png',
     link: '#',
     description: 'Um site responsivo para um restaurante, com funcionalidades de reserva e cardápio dinâmico.',
-    techs: [<FaHtml5 />, <FaCss3 />, <FaJs />],
+    techs: [<FaHtml5 key="html5" />, <FaCss3 key="css3" />, <FaJs key="js" />],
   },
   {
+    id: 2,
     title: 'Snake Shop',
     path: '/loja.png',
     link: '#',
     description: 'E-commerce para venda de produtos exóticos, com carrinho de compras e integração de pagamento.',
-    techs: [<FaHtml5 />, <FaCss3Alt />, <FaJs />],
+    techs: [<FaHtml5 key="html5" />, <FaCss3Alt key="css3alt" />, <FaJs key="js" />],
   },
   {
+    id: 3,
     title: 'Barbearia',
     path: '/barber.png',
     link: '#',
     description: 'Sistema de agendamento online para barbearia, com interface amigável e agenda dinâmica.',
-    techs: [<FaReact />, <SiNextdotjs />, <FaNodeJs />],
+    techs: [<FaReact key="react" />, <SiNextdotjs key="nextjs" />, <FaNodeJs key="nodejs" />],
   },
   {
+    id: 4,
     title: 'Plataforma de Cursos',
     path: '/onee.png',
     link: '#',
     description: 'Plataforma de vendas de cursos com suporte a múltiplos usuários e upload de aulas.',
-    techs: [<FaReact />, <FaNodeJs />, <SiTailwindcss />, <SiNextdotjs />, <SiSqlite />],
+    techs: [<FaReact key="react" />, <FaNodeJs key="nodejs" />, <SiTailwindcss key="tailwind" />, <SiNextdotjs key="nextjs" />, <SiSqlite key="sqlite" />],
   },
 ];
 
 const WorkSlider = () => {
   return (
     <Swiper
-      spaceBetween={20} // Espaçamento entre os itens
-      slidesPerView={2} // Mostrar 2 projetos por vez
-      pagination={{ clickable: true }} // Bolinhas de navegação
+      spaceBetween={20}
+      slidesPerView={2}
+      pagination={{ clickable: true }}
       modules={[Pagination]}
       className="py-10 px-4 sm:px-8 lg:px-16"
     >
-      {workSlides.map((project, index) => (
-        <SwiperSlide key={index}>
+      {workSlides.map((project) => (
+        <SwiperSlide key={project.id}>
           <a
             href={project.link}
             target="_blank"
             rel="noopener noreferrer"
             className="relative rounded-lg overflow-hidden group"
           >
-            {/* Imagem do Projeto */}
             <div className="w-full h-60 sm:h-80 lg:h-96 overflow-hidden relative">
               <Image
                 src={project.path}
@@ -66,14 +68,11 @@ const WorkSlider = () => {
               />
             </div>
 
-            {/* Informações do Projeto */}
             <div className="absolute inset-0 bg-black/70 p-4 flex flex-col justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
               <h3 className="text-white text-xl font-bold mb-2">{project.title}</h3>
               <p className="text-gray-300 text-sm mb-4">{project.description}</p>
               <div className="flex space-x-2 text-white text-lg">
-                {project.techs.map((tech, i) => (
-                  <span key={i}>{tech}</span>
-                ))}
+                {project.techs.map((tech) => tech)}
               </div>
             </div>
           </a>
@@ -84,3 +83,5 @@ const WorkSlider = () => {
 };
 
 export default WorkSlider;
+
+
